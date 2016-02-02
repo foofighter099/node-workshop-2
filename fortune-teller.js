574b7495777e61ab5a6e19a25ca48933693ef2af
foofighter0991 <andrewtognarini@hotmail.com>
2016-02-02 22:05:15 +0000

    fortune-teller now works!!

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,2 +1,14 @@
 var fortune = require("./library/fortune.js");
-fortune();
+//fortune();
+
+process.argv.forEach(function(val, index, array){
+        if (index === 2) {
+        Number(val);
+        for (var i = 0; i < val; i++) {
+        
+            fortune();
+        }
+        }
+});
+
+
